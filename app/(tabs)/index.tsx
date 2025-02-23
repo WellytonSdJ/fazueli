e8b74bf09f65d4e5613f4cf4dec7a7e0fdf61c08
Wellyton <wellytonsdj@gmail.com>
2025-02-23 18:07:14 -0300

    listagem e execucao dos audios

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,59 @@
 import { useEffect, useState } from 'react';
-import { View, StyleSheet, Button, Image } from 'react-native';
+import { View, StyleSheet, Button, Image, Platform, Text } from 'react-native';
+import * as FileSystem from 'expo-file-system';
 import { useAudioPlayer } from 'expo-audio';
 
-// const audioSource = require('@/assets/media/bordao.mp3');
+interface Audio {
+  bordao: string;
+  dilera: string;
+  explosao: string;
+  luanGameplays: string;
+  vemSeLascar: string;
+}
+
+const mediaPath = '@/assets/media/';
+
+const audioSources: Audio = {
+  bordao: require(`${mediaPath}bordao.mp3`),
+  dilera: require(`${mediaPath}dilera.mp3`),
+  explosao: require(`${mediaPath}explosao.mp3`),
+  luanGameplays: require(`${mediaPath}luan-gameplays.mp3`),
+  vemSeLascar: require(`${mediaPath}vem-se-lascar.mp3`),
+};
 
 export default function App() {
-  const [audioSource, setAudioSource] = useState(null)
-  const [audioList, setAudioList] = useState([])
+  const [currentAudio, setCurrentAudio] = useState<string>(audioSources.bordao);
 
-  useEffect(() => {
-    const fetchAudioFiles = async () => {
-      try {
-        const mediaPath = require.context('@/assets/media', false, /\.(mp3|wav)$/);
-        const files: { name: string; path: string }[] = mediaPath.keys().map((key) => ({
-          name: key.replace('./', ''),
-          path: mediaPath(key),
-        }));
-        setAudioList(files);
-      } catch (error) {
-        console.error('Error fetching audio files:', error);
-      }
-    };
+  const nextAudio = () => {
+  // Get the keys of the audioSources object as an array
+  const audioKeys = Object.keys(audioSources) as Array<keyof Audio>;
+  // Find the index of the current audio in the keys array
+  const currentIndex = audioKeys.findIndex((key) => audioSources[key] === currentAudio);
+  // Calculate the index of the next audio, wrapping around to the first if at the end
+  const nextIndex = (currentIndex + 1) % audioKeys.length;
+  console.log({nextIndex});
 
-    fetchAudioFiles();
-  }, []);
+  player.pause();
 
-  console.log(audioList)
+  player.replace(audioSources[audioKeys[nextIndex]])
+  // Set the currentAudio to the next audio
+  console.log(audioSources[audioKeys[nextIndex]]);
+  setCurrentAudio(audioSources[audioKeys[nextIndex]]);
 
-  const player = useAudioPlayer(audioSource);
+  }
 
+  const player = useAudioPlayer(currentAudio);
+  
   return (
-    <View style={styles.container}>
-      {/* <Image source={require('@/assets/media/letra-l.png')} style={{ width: 300, height: 300 }} />       */}
-      {/* <Button title="Apenas Faça" onPress={() => player.play()} /> */}
-      <h1>Apenas faça!</h1>
+    <View style={styles.container}>      
+      <View style={{ alignItems: 'center' }}>
+        <Image source={require('@/assets/images/letra-l.png')} style={{ width: 300, height: 300 }} />
+      </View>
+
+      <Button title="Apenas Faça" onPress={() => player.play()}  />
+      <Button title="Apenas TROQUE" onPress={() => nextAudio()}  />
+
+      <Text style={{ fontSize: 24 }}>Apenas faça!</Text>
     </View>
   );
 }
@@ -42,7 +62,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#f30101',
+    backgroundColor: '#f31d01f5',
     padding: 10,
+    gap: 8
   },
 });
